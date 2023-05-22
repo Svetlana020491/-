501cb9f3da26adbf6f5d93792aac516c2d5d6a5c
Svetlana Golubeva <svetlana020491@gmail.com>
2023-05-22 16:08:15 +0500

    задача4

---
+++ задача4/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// напишите программу вычесления значения функции возведения числа в квадрат
+
+int Square(int number)
+{
+    int result = number * number;
+    return result;
+}
+
+void Task0()
+{
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{number} * {number} = {Square(number)}");
+
+}
