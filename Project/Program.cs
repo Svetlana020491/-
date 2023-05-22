28781db3898f841d8f45d3ccdb8a6b2796e39e0b
Svetlana Golubeva <svetlana020491@gmail.com>
2023-05-22 13:22:53 +0500

    $

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = -7;
-Console.WriteLine(numberA);
-double result = numberA * numberA;
-Console.WriteLine(result);
+﻿// напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя)
+int numberA = 155;
+// Console.WriteLin(numberA);
+int result = numberA * numberA;
+//Console.WriteLine(result);
+Console.WriteLine($"квадрат числа {numberA} равен {result}");
