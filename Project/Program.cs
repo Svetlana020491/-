565a78d58284fc538a4ef8d60aed26762c7926a1
Svetlana Golubeva <svetlana020491@gmail.com>
2023-05-22 13:34:40 +0500

    Решение 1й задачи

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,8 @@
 ﻿// напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя)
-int numberA = 155;
-// Console.WriteLin(numberA);
+Console.WriteLine("Введите число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
 int result = numberA * numberA;
-//Console.WriteLine(result);
+
 Console.WriteLine($"квадрат числа {numberA} равен {result}");
